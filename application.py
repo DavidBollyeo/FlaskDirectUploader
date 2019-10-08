6d66fd7276162679382971e531f73931f9957528
David Bollyeo <david@bollyeo.com>
2019-10-08 13:36:11 +0100

    Initial modifaication add pymongo to save datat to

--- application.py
+++ application.py
@@ -9,6 +9,25 @@
 
 from flask import Flask, render_template, request, redirect, url_for
 import os, json, boto3
+import pymongo
+
+
+uri = os.environ.get('MONGODB_URI')
+
+client = pymongo.MongoClient(uri, connect=False)
+database = client.get_default_database()
+collection = database['stars']
+
+
+def update_account(username: str, full_name: str, avatar_url: str):
+    collection.insert_one(
+        {
+          'username': username,
+          'full_name': full_name,
+          'avatar_url': avatar_url
+        }
+    )
+
 
 app = Flask(__name__)
 
@@ -16,23 +35,23 @@ app = Flask(__name__)
 # Listen for GET requests to yourdomain.com/account/
 @app.route("/account/")
 def account():
-  # Show the account-edit HTML page:
-  return render_template('account.html')
+    # Show the account-edit HTML page:
+    return render_template('account.html')
 
 
 # Listen for POST requests to yourdomain.com/submit_form/
 @app.route("/submit-form/", methods = ["POST"])
 def submit_form():
-  # Collect the data posted from the HTML form in account.html:
-  username = request.form["username"]
-  full_name = request.form["full-name"]
-  avatar_url = request.form["avatar-url"]
+    # Collect the data posted from the HTML form in account.html:
+    username = request.form["username"]
+    full_name = request.form["full-name"]
+    avatar_url = request.form["avatar-url"]
 
-  # Provide some procedure for storing the new details
-  update_account(username, full_name, avatar_url)
+    # Provide some procedure for storing the new details
+    update_account(username, full_name, avatar_url)
 
-  # Redirect to the user's profile page, if appropriate
-  return redirect(url_for('profile'))
+    # Redirect to the user's profile page, if appropriate
+    return redirect(url_for('profile'))
 
 
 # Listen for GET requests to yourdomain.com/sign_s3/
@@ -41,36 +60,36 @@ def submit_form():
 # Python 3 for this view.
 @app.route('/sign-s3/')
 def sign_s3():
-  # Load necessary information into the application
-  S3_BUCKET = os.environ.get('S3_BUCKET')
+    # Load necessary information into the application
+    S3_BUCKET = os.environ.get('S3_BUCKET')
 
-  # Load required data from the request
-  file_name = request.args.get('file-name')
-  file_type = request.args.get('file-type')
+    # Load required data from the request
+    file_name = request.args.get('file-name')
+    file_type = request.args.get('file-type')
 
-  # Initialise the S3 client
-  s3 = boto3.client('s3')
+    # Initialise the S3 client
+    s3 = boto3.client('s3')
 
-  # Generate and return the presigned URL
-  presigned_post = s3.generate_presigned_post(
-    Bucket = S3_BUCKET,
-    Key = file_name,
-    Fields = {"acl": "public-read", "Content-Type": file_type},
-    Conditions = [
-      {"acl": "public-read"},
-      {"Content-Type": file_type}
-    ],
-    ExpiresIn = 3600
-  )
+    # Generate and return the presigned URL
+    presigned_post = s3.generate_presigned_post(
+      Bucket = S3_BUCKET,
+      Key = file_name,
+      Fields = {"acl": "public-read", "Content-Type": file_type},
+      Conditions = [
+        {"acl": "public-read"},
+        {"Content-Type": file_type}
+      ],
+      ExpiresIn = 3600
+    )
 
-  # Return the data to the client
-  return json.dumps({
-    'data': presigned_post,
-    'url': 'https://%s.s3.amazonaws.com/%s' % (S3_BUCKET, file_name)
-  })
+    # Return the data to the client
+    return json.dumps({
+      'data': presigned_post,
+      'url': 'https://%s.s3.amazonaws.com/%s' % (S3_BUCKET, file_name)
+    })
 
 
 # Main code
 if __name__ == '__main__':
-  port = int(os.environ.get('PORT', 5000))
-  app.run(host='0.0.0.0', port = port)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port = port)
